85fd2bcfd67ef66c57a38b0be9005c5b3436fc51
Bongorian <kentaroyamazaki.second@gmail.com>
2018-12-08 03:28:02 +0900

    add init led

--- flcl_midi_dirm.ino
+++ flcl_midi_dirm.ino
@@ -79,7 +79,48 @@ void setup()
   //led init
   strip.begin();
   strip.show();
+  rainbowCycle(20);
+  strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.setPixelColor(1, strip.Color(0, 0, 0));
+  strip.setPixelColor(2, strip.Color(0, 0, 0));
+  strip.setPixelColor(3, strip.Color(0, 0, 0));
   delay(1000);
+}
+
+void rainbowCycle(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++)
+  { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++)
+    {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85)
+  {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  else
+  {
+    if (WheelPos < 170)
+    {
+      WheelPos -= 85;
+      return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    else
+    {
+      WheelPos -= 170;
+      return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+  }
 }
 
 void checkSwitch()
